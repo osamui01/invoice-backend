1d8691f7d818f64c44e793b9f8e62d297e50f37a
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-03-17 09:33:58 +0000

    Added cors headers & DRF

--- backend/settings.py
+++ backend/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'carehomes',
     'residents',
     'services',
@@ -40,6 +42,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
 ]
 
 ROOT_URLCONF = 'backend.urls'
