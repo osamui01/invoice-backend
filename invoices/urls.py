66d95bcf57f709578f9f11a5cac5ad618beb913f
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-03-19 12:03:48 +0000

    Updated invoice item endpoint to use correct viewset

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
-from invoices.views import InvoiceViewSet
+from invoices.views import InvoiceViewSet, InvoiceItemViewSet
 
 
 invoice_router = routers.DefaultRouter()
 invoice_router.register(r'invoices', InvoiceViewSet, basename='invoices')
-invoice_router.register(r'invoice-items', InvoiceViewSet, basename='invoice-items')
+invoice_router.register(r'invoice-items', InvoiceItemViewSet, basename='invoice-items')
